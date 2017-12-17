3e35f32b6e7ac62fc066bf08f957c78a3d1a436d
Dirk Eddelbuettel <edd@debian.org>
2017-12-17 15:59:19 +0000

    version 0.3.3

--- inst/examples/cow.r
+++ inst/examples/cow.r
@@ -1,10 +1,10 @@
 #!/usr/bin/env r
 #
-# Minimal rhub::check_for_cran() wrapper
+# Minimal rhub::check_on_windows() wrapper
 #
 # Copyright (C) 2016 - 2017  Dirk Eddelbuettel
 #
 # Released under GPL (>= 2)
 
-rhub::check_for_cran()
+rhub::check_on_windows()
 
