472d2668bdb698552619788906cd96cf1c3a32a3
Dirk Eddelbuettel <edd@debian.org>
2019-06-09 15:30:03 +0000

    version 0.3.8

--- inst/examples/install.r
+++ inst/examples/install.r
@@ -5,7 +5,7 @@
 # Note that a more featureful version exists in install2.r
 # with an added dependency on the 'docopt' argument parser
 #
-# Copyright (C) 2006 - 2018  Dirk Eddelbuettel
+# Copyright (C) 2006 - 2019  Dirk Eddelbuettel
 #
 # Released under GPL (>= 2)
 
@@ -16,9 +16,11 @@ if (is.null(argv) | length(argv) < 1) {
         argv <- "."
     } else {
         cat("Usage: installr.r pkg1 [pkg2 pkg3 ...]\n\n")
-        cat("Set environment variables REPOS and LIBLOC to overrride defaults.\n")
         cat("Installs pkg1, ... from existing files with matching extension.\n")
-        cat("Use \".\" to install the package in current working directory.\n")
+        cat("Use \".\" to install the package in current working directory.\n\n")
+        cat("Use (remote) packages pkgname1 pkgname2 pkgname3 ... for parallel\n")
+        cat("installation if 'Ncpus' option is set (or multiple cores present).\n\n")
+        cat("Set environment variables REPOS and LIBLOC to overrride defaults.\n\n")
         q()
     }
 }
@@ -33,15 +35,30 @@ if (Sys.getenv("REPOS") == "NULL") repos = NULL
 lib.loc <- Sys.getenv("LIBLOC", unset="/usr/local/lib/R/site-library")
 
 ## helper function to for existing files with matching extension
-isMatchingFile <- function(f) (file.exists(f) &&
-                               grepl("(\\.tar\\.gz|\\.tgz|\\.zip)$", f)) || (f == ".")
+isMatchingFile <-
+    function(f) (file.exists(f) &&
+                 grepl("(\\.tar\\.gz|\\.tgz|\\.zip)$", f)) || (f == ".")
 
 ## helper function which switches to local (ie NULL) repo if matching file is presented
-installArg <- function(f, lib, rep) install.packages(pkgs=f, lib=lib,
-                                                     repos=if (isMatchingFile(f)) NULL else repos)
+installArg <-
+    function(f, lib, rep) install.packages(pkgs=f,
+                                           lib=lib,
+                                           repos=if (isMatchingFile(f)) NULL else repos)
 
-## use argv [filtered by installArg()], lib.loc and repos to install the packages
-sapply(argv, installArg, lib.loc, repos)
+
+## check arguments for local files -- if none, then we can pass vector on
+isLocal <- sapply(argv, isMatchingFile)
+
+if (any(isLocal)) {
+    ## use argv [filtered by installArg()], lib.loc and repos to install the packages
+    sapply(argv, installArg, lib.loc, repos)
+} else {
+    ## all remote, so pass argv so Ncpus can enter
+    install.packages(pkgs=argv,
+                     lib=lib.loc,
+                     repos=repos,
+                     Ncpus=getOption("Ncpus", max(1L, parallel::detectCores())))
+}
 
 ## clean up any temp file containing CRAN directory information
 sapply(list.files(path=tempdir(), pattern="^(repos|libloc).*\\.rds$", full.names=TRUE), unlink)
---
+++ inst/examples/tt.r
@@ -0,0 +1,65 @@
+#!/usr/bin/env r
+#
+# tinytest wrapper
+#
+# Copyright (C) 2019         Dirk Eddelbuettel
+#
+# Released under GPL (>= 2)
+
+if (!requireNamespace("tinytest", quietly=TRUE))
+    stop("Please install 'inytest' from CRAN.", call. = FALSE)
+
+## load docopt and tinytest from CRAN
+suppressMessages({
+    library(docopt)               # we need docopt (>= 0.3) as on CRAN
+    library(tinytest)             # or can use devtools as a fallback
+})
+
+## load docopt package from CRAN
+library(docopt)
+
+## configuration for docopt
+doc <- "Usage: tt.r [-h] [-x] [-a] [-b] [-f] [-d] [-p] [-s] [ARG...]
+
+-a --all            use test_all mode [default: FALSE]
+-b --build          use build-install-test mode [default: FALSE]
+-f --file           use file mode [default: FALSE]
+-d --directory      use directory mode [default: FALSE]
+-p --package        use package mode [default: FALSE]
+-s --silent         use silent and do not print result [default: FALSE]
+-h --help           show this help text
+-x --usage          show help and short example usage"
+opt <- docopt(doc)			# docopt parsing
+
+if (opt$usage) {
+    cat(doc, "\n\n")
+    cat("where ARG... can be one or more file name, or directories or package names.
+
+Examples:
+  tt.r -a                             # test 'all files' from current directory
+  tt.r -f testfile.R                  # test the file 'testfile.R'
+  tt.t -d testdir                     # test all files in the directory 'testdir'
+  tt.r -p testpkg                     # test the (installed) package 'testpkg'
+  tt.r                                # run test_all() iff DESCRIPTION && inst/tinytest/
+
+tt.r is part of littler which brings 'r' to the command-line.
+See http://dirk.eddelbuettel.com/code/littler.html for more information.\n")
+    q("no")
+}
+
+res <- NULL
+if (opt$all) {
+    res <- test_all(if (length(opt$ARG) == 0) "." else opt$ARG)
+} else if (opt$build) {
+    res <- build_install_test(if (length(opt$ARG) == 0) "." else opt$ARG)
+} else if (opt$file) {
+    res <- run_test_file(opt$ARG)
+} else if (opt$directory) {
+    res <- run_test_dir(opt$ARG)
+} else if (opt$package) {
+    res <- test_package(opt$ARG)
+} else if (file.exists("DESCRIPTION") && dir.exists("inst/tinytest")) {
+    res <- test_all(".")
+}
+
+if (!opt$silent && !is.null(res)) print(res)
